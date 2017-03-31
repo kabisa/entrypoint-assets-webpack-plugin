7c6bf616d91edaf6d78076206476701612cb4ab6
Klavs Prieditis <klavs.prieditis@geta.no>
2017-03-31 11:59:23 +0300

    add option to remove duplicate

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ function EntrypointAssetsPlugin(options) {
         "map": /\.map$/,
         "css": /\.css$/
     }, options.mappings)
+    this.removeDuplicateChunks = !!options.removeDuplicateChunks
 }
 
 EntrypointAssetsPlugin.prototype.apply = function(compiler) {
     const filename = this.filename
     const mappings = this.mappings
+    const removeDuplicateChunks = this.removeDuplicateChunks
     compiler.plugin("emit", function(compilation, callback) {
         const publicPath = compilation.mainTemplate.getPublicPath({
             hash: compilation.hash
@@ -20,7 +22,8 @@ EntrypointAssetsPlugin.prototype.apply = function(compiler) {
         const entrypoints = {}
         Object.keys(compilation.entrypoints).forEach(name => {
             const ep = compilation.entrypoints[name]
-            const assets = ep.chunks
+            const chunks = removeDuplicateChunks ? ep.chunks.filter((chunk, pos, self) => self.indexOf(chunk) === pos) : ep.chunks
+            const assets = chunks
                 .reduce((array, c) => array.concat(c.files || []), [])
                 .map(asset => publicPath + asset)
             entrypoints[name] = {}
